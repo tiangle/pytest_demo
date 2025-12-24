3cfda05fff35debb35216abe08e1e3fb65ecf590
8586 <8586@dahantc.com>
2025-12-24 11:19:50 +0800

    关闭session

--- api/SmaApi.py
+++ api/SmaApi.py
@@ -9,10 +9,11 @@ from exceptions.exceptions import ApiRequestError
 class SmaApi:
     def __init__(self):
         self.timeout = CONFIG.TIME_OUT
+        self.session = requests.Session()
 
     def submit(self, url, body):
         try:
-            response = requests.post(url, json=body, timeout=self.timeout)
+            response = self.session.post(url, json=body, timeout=self.timeout)
             response.raise_for_status()
             response_test = json.loads(response.text)
             response_result = int(response_test["result"])
@@ -20,3 +21,7 @@ class SmaApi:
             return {"result": response_result, "desc": response_desc}
         except Exception as e:
             raise ApiRequestError from e
+
+    def __del__(self):
+        if hasattr(self, "session"):
+            self.session.close()
